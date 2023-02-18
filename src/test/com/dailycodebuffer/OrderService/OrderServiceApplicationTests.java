94bfc1afed6740574a6541eac5dc310f3669fdbd
Rabia <sdetcode@gmail.com>
2023-02-18 18:19:03 +0530

    initial setup

---
+++ src/test/com/dailycodebuffer/OrderService/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package OrderService.src.test.com.dailycodebuffer.OrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
